445b46913a0ddb08b1bde207b953ec48288e9689
Chris Morrell <inxilpro@users.noreply.github.com>
2021-10-11 16:09:50 -0400

    Updates from gretel

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,16 +8,6 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
 {
-	protected function setUp() : void
-	{
-		parent::setUp();
-		
-		$config = $this->app['config'];
-		
-		// Add encryption key for HTTP tests
-		$config->set('app.key', 'base64:tfsezwCu4ZRixRLA/+yL/qoouX++Q3lPAPOAbtnBCG8=');
-	}
-	
 	protected function getPackageProviders($app)
 	{
 		return [
@@ -28,5 +18,10 @@ abstract class TestCase extends Orchestra
 	protected function getPackageAliases($app)
 	{
 		return [];
+	}
+	
+	protected function getApplicationTimezone($app)
+	{
+		return 'America/New_York';
 	}
 }
